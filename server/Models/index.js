c1bedc63c73d39123f621bb19a7e770ca38279a8
RashmiBalaji <rashmimenonv@gmail.com>
2020-11-06 12:19:59 +0100

    commit

--- server/Models/index.js
+++ server/Models/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 const conf = require("../config");
 
-mongoose.connect(MONGODB_URI || `mongodb://localhost:27017/${conf.dbName}`, {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/${conf.dbName}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });

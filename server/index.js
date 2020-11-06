22ed60308a0c9c794f1881eddd5326e950fa0e79
RashmiBalaji <rashmimenonv@gmail.com>
2020-11-06 12:58:33 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -16,15 +16,7 @@ app.use(express.static('../../client/build'));
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('../../client/build'));
-  // //
-  // app.get('*', (req, res) => {
-  //   res.sendfile(path.join(__dirname = 'client/build/index.html'));
-  // })
 }
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
 
 //Setting up a socket with the namespace "connection" for new sockets
 io.on("connection", (socket) => {
